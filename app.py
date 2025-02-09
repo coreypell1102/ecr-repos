067877793a572d20912b7fa8eb0ef9a22c2b1e97
Corey Pellizzari <pellizzari.corey@gmail.com>
2025-02-08 23:37:12 -0500

    fixed a bunch of stuff

--- app.py
+++ app.py
@@ -11,33 +11,33 @@ from infrastructure.pipeline import PipelineStack
 app = cdk.App()
 
 #Define possible app environments
-app_envs = {
+app_env = {
     "211125306454": {"ctx": "dev", "region": "us-east-1"},
     "123456789012": {"ctx": "prod", "region": "us-east-1"},
 }
 
-sts_client = boto3.Session().client('sts')
+sts_client = boto3.Session().client("sts")
 account = None
 
 try:
     account = sts_client.get_caller_identity().get("Account")
 except NoCredentialsError:
-    print("No AWS credentials found. Please configure your credentials.")
+    print("No valid credentials found")
 except Exception as e:
-    print("An error occurred: ", e)
-    
+    print(f"Error: {e}")
+
 if not account:
-    print("Assuming this is being run locally and will use the dev account")
-    account = next(iter(app_envs)) # Use the first account in the list
+    print(f"Assuming this is being run locally and will use 'dev' account")
+    account = next(iter(app_env))  # dev account
 
-print(f"Using account: {account}")
-print(f"Using environment: {app_envs[account]}")
+print(f"Account: {account}")
+print(f"Using environment: {app_env[account]}")
 
-env = cdk.Environment(account=account, region=app_envs[account]["region"])
+env = cdk.Environment(account=account, region=app_env[account]["region"])
 
-ctx = app.node.try_get_context(app_envs[account]["ctx"])
+ctx = app.node.try_get_context(app_env[account]["ctx"])
 if ctx is None:
-    raise ValueError(f"Context {app_envs[account]['ctx']} not found in cdk.context.json")
+    raise ValueError(f"Context for '{app_env[account]['ctx']}' not found! Check your cdk.json file.")
 
 pipeline = PipelineStack(
     app,
@@ -47,5 +47,9 @@ pipeline = PipelineStack(
     stack_name=f"{ctx['app_name']}-Pipeline",
     description=f"{ctx['app_name']} Infrastructure Pipeline",
 )
+
+
+# Add tags to the entire app (all resources created by this app)
+cdk.Tags.of(pipeline).add("APPLICATION", ctx["app_name"])
 
 app.synth()
